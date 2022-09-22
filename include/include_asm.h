91cb33200936a83aa7901117d6c9ab386e7a9067
angie <angheloalf95@gmail.com>
2022-09-21 22:24:04 -0300

    Fix include asm

---
+++ include/include_asm.h
@@ -0,0 +1,29 @@
+#ifndef INCLUDE_ASM_H
+#define INCLUDE_ASM_H
+
+#if !defined(SPLAT) && !defined(__CTX__) && !defined(PERMUTER)
+#ifndef INCLUDE_ASM
+#define INCLUDE_ASM(TYPE, FOLDER, NAME, ARGS...) \
+   __asm__( \
+        ".section .text\n" \
+        "\t.set noat\n" \
+        "\t.set noreorder\n" \
+        "\t.align\t2\n" \
+        "\t.globl\t"#NAME"\n" \
+        "\t.type "#NAME", @function\n" \
+        "\t.ent\t"#NAME"\n" \
+        #NAME ":\n" \
+        "\t.include \"asm/nonmatchings/"FOLDER"/"#NAME".s\"\n" \
+        "\t.set reorder\n" \
+        "\t.set at\n" \
+        "\t.end\t"#NAME"\n" \
+        ".end"#NAME":\n" \
+        "\t.size\t"#NAME",.end"#NAME"-"#NAME \
+    );
+#endif
+__asm__(".include \"include/labels.inc\"\n");
+#else
+#define INCLUDE_ASM(TYPE, FOLDER, NAME, ARGS...)
+#endif
+
+#endif
---
+++ src/boot/10F0.c
@@ -0,0 +1,9 @@
+#include "ultra64.h"
+#include "include_asm.h"
+
+
+INCLUDE_ASM(s32, "boot/10F0", func_800004F0);
+
+INCLUDE_ASM(s32, "boot/10F0", func_80000580);
+
+INCLUDE_ASM(s32, "boot/10F0", func_80000604);
