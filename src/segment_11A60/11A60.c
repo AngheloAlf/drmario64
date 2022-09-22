0974d6a3acfc403fef4ec763fa2773d63d4db5bb
angie <angheloalf95@gmail.com>
2022-09-22 02:08:15 -0300

    11A60

---
+++ src/segment_11A60/11A60.c
@@ -0,0 +1,9 @@
+#include "libc/stdint.h"
+#include "segment_symbols.h"
+
+uintptr_t D_80029C40[] = {
+    SEGMENT_START(segment_11A70),
+    SEGMENT_START(segment_11A70),
+    0x800B3640,
+    0x80124610,
+};
