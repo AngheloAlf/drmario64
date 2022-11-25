18cf2bc9167b71133f4cfb7dbf4f94a496e5ff3c
angie <angheloalf95@gmail.com>
2022-11-25 13:59:57 -0300

    a few c files

---
+++ src/main_segment/0131D0.c
@@ -0,0 +1,22 @@
+#include "ultra64.h"
+#include "include_asm.h"
+#include "macros_defines.h"
+#include "unknown_structs.h"
+#include "unk.h"
+
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/0131D0", func_8003CE20);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/0131D0", func_8003CE5C);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/0131D0", func_8003CF2C);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/0131D0", func_8003CFA8);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/0131D0", func_8003D110);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/0131D0", func_8003D41C);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/0131D0", func_8003D4C8);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/0131D0", func_8003D6D0);
---
+++ src/main_segment/053610.c
@@ -0,0 +1,10 @@
+#include "ultra64.h"
+#include "include_asm.h"
+#include "macros_defines.h"
+#include "unknown_structs.h"
+#include "unk.h"
+
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/053610", func_8007D260);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/053610", func_8007D348);
