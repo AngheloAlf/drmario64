889c844fa67961b71b67c72a110c3501184a3f67
angie <angheloalf95@gmail.com>
2022-11-28 20:20:39 -0300

    more splits

---
+++ src/main_segment/043C20.c
@@ -0,0 +1,62 @@
+#include "ultra64.h"
+#include "include_asm.h"
+#include "macros_defines.h"
+#include "unknown_structs.h"
+#include "unk.h"
+
+
+INCLUDE_RODATA("asm/nonmatchings/main_segment/043C20", D_800B22B0);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/043C20", func_8006D870);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/043C20", func_8006D91C);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/043C20", func_8006E0EC);
+
+
+INCLUDE_RODATA("asm/nonmatchings/main_segment/043C20", RO_800B2314);
+INCLUDE_RODATA("asm/nonmatchings/main_segment/043C20", RO_800B2330);
+
+INCLUDE_RODATA("asm/nonmatchings/main_segment/043C20", D_800B233C);
+INCLUDE_RODATA("asm/nonmatchings/main_segment/043C20", D_800B2340);
+INCLUDE_RODATA("asm/nonmatchings/main_segment/043C20", D_800B2344);
+INCLUDE_RODATA("asm/nonmatchings/main_segment/043C20", D_800B2348);
+INCLUDE_RODATA("asm/nonmatchings/main_segment/043C20", D_800B2354);
+INCLUDE_RODATA("asm/nonmatchings/main_segment/043C20", D_800B2358);
+
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/043C20", func_8006E80C);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/043C20", func_8006E884);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/043C20", func_8006F628);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/043C20", func_8006F684);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/043C20", func_8006F950);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/043C20", func_8006FFD0);
+
+INCLUDE_RODATA("asm/nonmatchings/main_segment/043C20", D_800B23C4);
+INCLUDE_RODATA("asm/nonmatchings/main_segment/043C20", RO_800B23CC);
+INCLUDE_RODATA("asm/nonmatchings/main_segment/043C20", D_800B23D4);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/043C20", func_8007023C);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/043C20", func_80070360);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/043C20", func_80071624);
+
+void func_80071A44(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/043C20", func_80071A4C);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/043C20", func_80071CE0);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/043C20", func_80071EF0);
+
+void func_80071F0C(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/043C20", func_80071F14);
