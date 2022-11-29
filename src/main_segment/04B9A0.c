fe757997cb2a7b528d5d54bfaad60403e33a239e
Angie <angheloalf95@gmail.com>
2022-11-28 21:15:55 -0300

    splits

---
+++ src/main_segment/04B9A0.c
@@ -0,0 +1,17 @@
+#include "ultra64.h"
+#include "include_asm.h"
+#include "macros_defines.h"
+#include "unknown_structs.h"
+#include "unk.h"
+
+
+INCLUDE_RODATA("asm/nonmatchings/main_segment/04B9A0", D_800B31C0);
+INCLUDE_RODATA("asm/nonmatchings/main_segment/04B9A0", D_800B31D8);
+INCLUDE_RODATA("asm/nonmatchings/main_segment/04B9A0", D_800B31DC);
+INCLUDE_RODATA("asm/nonmatchings/main_segment/04B9A0", D_800B31E0);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/04B9A0", func_800755F0);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/04B9A0", func_80075A2C);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/04B9A0", func_80075CF8);
