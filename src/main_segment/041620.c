bd5637490e9578ea4c5148c29200a8398c24cb58
angie <angheloalf95@gmail.com>
2022-11-29 11:14:32 -0300

    bunch of splits

---
+++ src/main_segment/041620.c
@@ -0,0 +1,51 @@
+#include "ultra64.h"
+#include "include_asm.h"
+#include "macros_defines.h"
+#include "unknown_structs.h"
+#include "unk.h"
+
+
+
+INCLUDE_RODATA("asm/nonmatchings/main_segment/041620", D_800B2160);
+INCLUDE_RODATA("asm/nonmatchings/main_segment/041620", D_800B2168);
+INCLUDE_RODATA("asm/nonmatchings/main_segment/041620", D_800B2170);
+INCLUDE_RODATA("asm/nonmatchings/main_segment/041620", D_800B2178);
+INCLUDE_RODATA("asm/nonmatchings/main_segment/041620", D_800B2180);
+INCLUDE_RODATA("asm/nonmatchings/main_segment/041620", D_800B2184);
+INCLUDE_RODATA("asm/nonmatchings/main_segment/041620", D_800B2188);
+INCLUDE_RODATA("asm/nonmatchings/main_segment/041620", D_800B218C);
+INCLUDE_RODATA("asm/nonmatchings/main_segment/041620", D_800B21D0);
+
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/041620", func_8006B270);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/041620", func_8006B8E0);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/041620", func_8006BBEC);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/041620", func_8006BC5C);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/041620", func_8006BD00);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/041620", func_8006BDFC);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/041620", func_8006C1D0);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/041620", func_8006C580);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/041620", func_8006C6FC);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/041620", func_8006C88C);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/041620", func_8006D0E8);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/041620", func_8006D150);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/041620", func_8006D254);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/041620", func_8006D620);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/041620", func_8006D7B0);
+
+INCLUDE_RODATA("asm/nonmatchings/main_segment/041620", D_800B2294);
+INCLUDE_RODATA("asm/nonmatchings/main_segment/041620", D_800B22AC);
