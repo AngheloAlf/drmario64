a9fcfbdd800a69db669a93a4e2bdfc716b5cabaa
angie <angheloalf95@gmail.com>
2022-12-03 14:32:53 -0300

    15.0155%

--- src/main_segment/004BE0.c
+++ src/main_segment/004BE0.c
@@ -2,9 +2,18 @@
 #include "include_asm.h"
 #include "macros_defines.h"
 #include "unknown_structs.h"
-#include "unk.h"
+#include "main_segment_functions.h"
+#include "main_segment_variables.h"
 
 
-INCLUDE_ASM("asm/nonmatchings/main_segment/004BE0", func_8002E830);
+void func_8002E830(u32 arg0) {
+    B_800E4640[0] = arg0;
+    D_80088480 = 1;
+
+    while (D_80088480 < ARRAY_COUNT(B_800E4640)) {
+        B_800E4640[D_80088480] = B_800E4640[D_80088480 - 1] * 69069;
+        D_80088480++;
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/main_segment/004BE0", func_8002E8B0);
