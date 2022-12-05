16c48b75a507160168b1621a1f079c1576258ca4
Angie <angheloalf95@gmail.com>
2022-12-05 20:33:05 -0300

    fix url path on ci

--- tools/upload_progress.py
+++ tools/upload_progress.py
@@ -6,7 +6,6 @@
 import argparse
 import subprocess
 import requests
-from pathlib import Path
 
 import progress
 
@@ -18,7 +17,7 @@ def get_git_commit_sha() -> str:
     return subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
 
 def generate_url(base_url: str, project: str) -> str:
-    return str(Path(base_url) / "data" / project)
+    return "/".join([base_url, "data", project])
 
 
 def uploadProgressMain():
