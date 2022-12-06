3760ddf43e018dd1848ab5fdfe861deb8cb95743
angie <angheloalf95@gmail.com>
2022-12-05 22:17:43 -0300

    check_format.sh
    Co-authored-by: rylieb <31797144+rylieb@users.noreply.github.com>

---
+++ tools/check_format.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+STATUSOLD=`git status --porcelain`
+make format
+if [ $? -ne 0 ]
+then
+   echo "Formatter failed. Exiting."
+   exit -1
+fi
+make tidy
+if [ $? -ne 0 ]
+then
+   echo "Formatter failed. Exiting."
+   exit -1
+fi
+STATUSNEW=`git status --porcelain`
+
+if [ "${STATUSOLD}" != "${STATUSNEW}" ];
+then
+    echo ""
+    echo "Misformatted files found. Run `make format` and `make tidy` and verify codegen is not impacted."
+    echo ""
+    diff --unified=0  --label "Old git status" <(echo "${STATUSOLD}") --label "New git status" <(echo "${STATUSNEW}")
+    echo ""
+    echo "Exiting."
+    exit 1
+fi
+
+exit 0
