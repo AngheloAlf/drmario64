69ed27e9a6a1adf4f5a545505ec9197d36d24dc9
angie <angheloalf95@gmail.com>
2022-12-20 16:02:51 -0300

    char_anime.c

---
+++ src/main_segment/char_anime/0344D0.c
@@ -0,0 +1,35 @@
+/**
+ * Originally part of char_anime.c
+ */
+
+#include "ultra64.h"
+#include "include_asm.h"
+#include "macros_defines.h"
+#include "unknown_structs.h"
+#include "unk.h"
+
+INCLUDE_RODATA("asm/nonmatchings/main_segment/char_anime/0344D0", D_800B1A60);
+
+INCLUDE_RODATA("asm/nonmatchings/main_segment/char_anime/0344D0", D_800B1AB0);
+
+INCLUDE_RODATA("asm/nonmatchings/main_segment/char_anime/0344D0", D_800B1B00);
+
+INCLUDE_RODATA("asm/nonmatchings/main_segment/char_anime/0344D0", D_800B1B04);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/0344D0", func_8005E120);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/0344D0", func_8005E154);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/0344D0", func_8005E174);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/0344D0", func_8005E32C);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/0344D0", func_8005E358);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/0344D0", func_8005E36C);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/0344D0", func_8005E40C);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/0344D0", animeState_set);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/0344D0", animeState_update);
---
+++ src/main_segment/char_anime/034890.c
@@ -0,0 +1,37 @@
+/**
+ * Originally part of char_anime.c
+ */
+
+#include "ultra64.h"
+#include "include_asm.h"
+#include "macros_defines.h"
+#include "unknown_structs.h"
+#include "unk.h"
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/034890", func_8005E4E0);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/034890", func_8005E4FC);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/034890", func_8005E5B0);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/034890", func_8005E67C);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/034890", func_8005E750);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/034890", func_8005E998);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/034890", func_8005EAFC);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/034890", func_8005EBA8);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/034890", func_8005EBDC);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/034890", animeSmog_stop);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/034890", animeSmog_update);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/034890", func_8005ED74);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/034890", func_8005EE64);
+
+INCLUDE_ASM("asm/nonmatchings/main_segment/char_anime/034890", func_8005EE98);
