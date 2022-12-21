c1c29d186504db10020cf919ddc5293f20b6e639
angie <angheloalf95@gmail.com>
2022-12-21 20:18:48 -0300

    name lws.c

--- src/main_segment/lws.c
+++ src/main_segment/lws.c
@@ -1,7 +1,11 @@
+/**
+ * Original filename: lws.c
+ */
+
 #include "ultra64.h"
 #include "include_asm.h"
 #include "macros_defines.h"
 #include "unknown_structs.h"
 #include "unk.h"
 
-INCLUDE_ASM("asm/nonmatchings/main_segment/051A00", lws_anim);
+INCLUDE_ASM("asm/nonmatchings/main_segment/lws", lws_anim);
