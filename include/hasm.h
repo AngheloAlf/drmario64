93b617cbaea1150e6a3e249f875dd08768219060
angie <angheloalf95@gmail.com>
2022-12-30 14:59:51 -0300

    mmuldi3.s

--- include/hasm.h
+++ include/hasm.h
@@ -1,9 +1,10 @@
 #ifndef HASM_H
 #define HASM_H
 
+#ifdef _LANGUAGE_ASSEMBLY
 
 #define LEAF(x)                 \
-    .balign 4                  ;\
+    .align 2                   ;\
     .globl x                   ;\
     .type x, @function         ;\
     x:                         ;\
@@ -14,8 +15,24 @@
     .size x, . - x             ;\
     .end x
 
+.macro move dst, src
+    addu \dst, \src, $zero
+.endm
+
+// divu dst
+.macro divu_ds dst, dividend, divisor
+    .set noreorder
+    divu        $zero, \dividend, \divisor
+    bnez        \divisor, 0f
+     nop
+    break       7
+0:
+    mflo        \dst
+    .set reorder
+.endm
+
+#endif
 
 #define BOOT_STACK_SIZE 0x2000
-
 
 #endif
