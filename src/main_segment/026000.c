dcf42c7cd9212500afe58a8c313f8f7da52fcc99
angie <angheloalf95@gmail.com>
2023-01-31 19:07:36 -0300

    cn: 026000 and 026550_cn

--- src/main_segment/026000.c
+++ src/main_segment/026000.c
@@ -44,3 +44,44 @@ INCLUDE_ASM("asm/us/nonmatchings/main_segment/026000", func_8003E6D8);
 #if VERSION_US
 INCLUDE_ASM("asm/us/nonmatchings/main_segment/026000", func_8003E6F8);
 #endif
+
+
+#if VERSION_CN
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026000", func_800400C0_cn);
+#endif
+
+#if VERSION_CN
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026000", func_800400E8_cn);
+#endif
+
+#if VERSION_CN
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026000", func_80040158_cn);
+#endif
+
+#if VERSION_CN
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026000", func_800402AC_cn);
+#endif
+
+#if VERSION_CN
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026000", func_800402F8_cn);
+#endif
+
+#if VERSION_CN
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026000", func_80040388_cn);
+#endif
+
+#if VERSION_CN
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026000", func_80040390_cn);
+#endif
+
+#if VERSION_CN
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026000", func_8004058C_cn);
+#endif
+
+#if VERSION_CN
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026000", func_800405D4_cn);
+#endif
+
+#if VERSION_CN
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026000", func_80040604_cn);
+#endif
---
+++ src/main_segment/026550_cn.c
@@ -0,0 +1,416 @@
+#include "libultra.h"
+#include "include_asm.h"
+#include "macros_defines.h"
+#include "unknown_structs.h"
+
+#if VERSION_CN
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026550_cn", func_80040620_cn);
+
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026550_cn", func_800407C4_cn);
+
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026550_cn", func_80040ADC_cn);
+
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026550_cn", func_80040CA8_cn);
+
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026550_cn", func_80040EAC_cn);
+
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026550_cn", func_80041064_cn);
+
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026550_cn", func_80041444_cn);
+
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026550_cn", func_8004157C_cn);
+
+ASM_RODATA;
+
+const char STR_800C4200_cn[] = "MajorityRensa";
+
+const char STR_800C4210_cn[] = "WidErase";
+
+const char STR_800C421C_cn[] = "Rensa&Attack";
+
+const char STR_800C422C_cn[] = "Rensa&Erase";
+
+const char STR_800C4238_cn[] = "SmallRensa";
+
+const char STR_800C4244_cn[] = "FastErase";
+
+const char STR_800C4250_cn[] = "Normal";
+
+const char STR_800C4258_cn[] = "Badline1";
+
+const char STR_800C4264_cn[] = "Badline2";
+
+const char STR_800C4270_cn[] = "LastVsn";
+
+const char STR_800C4278_cn[] = "LastVsnh";
+
+const char STR_800C4284_cn[] = "Last";
+
+const char STR_800C428C_cn[] = "Narrow";
+
+const char STR_800C4294_cn[] = "Dead";
+
+const char STR_800C429C_cn[] = "On";
+
+const char STR_800C42A0_cn[] = "Off";
+
+const char STR_800C42A4_cn[] = "Plain4";
+
+const char STR_800C42AC_cn[] = "Plain3";
+
+const char STR_800C42B4_cn[] = "Plain2";
+
+const char STR_800C42BC_cn[] = "Plain1";
+
+const char STR_800C42C4_cn[] = "Peach";
+
+const char STR_800C42CC_cn[] = "Koopa";
+
+const char STR_800C42D4_cn[] = "Kamek";
+
+const char STR_800C42DC_cn[] = "PakkunFlower";
+
+const char STR_800C42EC_cn[] = "Telesa";
+
+const char STR_800C42F4_cn[] = "Hanachan";
+
+const char STR_800C4300_cn[] = "Heihoo";
+
+const char STR_800C4308_cn[] = "Chiropoo";
+
+const char STR_800C4314_cn[] = "PukuPuku";
+
+const char STR_800C4320_cn[] = "Bomhei";
+
+const char STR_800C4328_cn[] = "NokoNoko";
+
+const char STR_800C4334_cn[] = "Mario";
+
+const char STR_800C433C_cn[] = "FasterNW";
+
+const char STR_800C4348_cn[] = "NoWait";
+
+const char STR_800C4350_cn[] = "Fastest";
+
+const char STR_800C4358_cn[] = "Faster";
+
+const char STR_800C4360_cn[] = "Fast";
+
+const char STR_800C4368_cn[] = "Slow";
+
+const char STR_800C4370_cn[] = "Slower";
+
+const char STR_800C4378_cn[] = "SpSlow";
+
+const char STR_800C4380_cn[] = "PreemPtive";
+
+const char STR_800C438C_cn[] = "Rensa";
+
+const char STR_800C4394_cn[] = "Hard";
+
+const char STR_800C439C_cn[] = "Easy";
+
+const char STR_800C43A4_cn[] = "Pile";
+
+const char STR_800C43AC_cn[] = "Few Vir";
+
+const char STR_800C43B4_cn[] = "Many Vir";
+
+const char STR_800C43C0_cn[] = "Damage";
+
+const char STR_800C43C8_cn[] = "Random";
+
+const char STR_800C43D0_cn[] = "Unconditional";
+
+const char STR_800C43E0_cn[] = "--------------";
+
+const char STR_800C43F0_cn[] = "Lose";
+
+const char STR_800C43F8_cn[] = "be Erase";
+
+const char STR_800C4404_cn[] = "be Attack";
+
+const char STR_800C4410_cn[] = "Rapid";
+
+const char STR_800C4418_cn[] = "beNoWait";
+
+const char STR_800C4424_cn[] = "beSlow";
+
+const char STR_800C442C_cn[] = "beFast";
+
+const char STR_800C4434_cn[] = "Not Move";
+
+const char STR_800C4440_cn[] = "Waver";
+
+const char STR_800C4448_cn[] = "Rotate";
+
+const char STR_800C4450_cn[] = "----------";
+
+const char STR_800C445C_cn[] = "SlideFS";
+
+const char STR_800C4464_cn[] = "SlideS";
+
+const char STR_800C446C_cn[] = "DownS";
+
+const char STR_800C4474_cn[] = "VirusLv";
+
+const char STR_800C447C_cn[] = "@c2";
+
+const char STR_800C4480_cn[] = "%s\n\n\n\n\n\n";
+
+const char STR_800C448C_cn[] = "%6s\n\n";
+
+const char STR_800C4494_cn[] = "@c2S_er Slow Fast F_er Fest N_Wt F_NW";
+
+const char STR_800C44BC_cn[] = "@m%c%03d  ";
+
+const char STR_800C44C8_cn[] = "\n\n";
+
+const char STR_800C44CC_cn[] = "@c3<- CharacterMenu\t\t\t\t\tLogicMenu ->";
+
+const char STR_800C44F4_cn[] = "@c2Logic             State\n@c7 @m%c%02d:%-14s @m%c%02d:%-14s\n\n";
+
+const char STR_800C4534_cn[] = "@c2OLVrs  ErVrs  ErL1   HeiEr   WidEr   HeightP\n@c7";
+
+const char STR_800C4568_cn[] = " @m%c%05d%% ";
+
+const char STR_800C4578_cn[] = " @m%c%05d ";
+
+const char STR_800C4584_cn[] = "@c2\tALNB   FC     FV     C      V\n@c2Hei@c7  -----";
+
+const char STR_800C45B8_cn[] = "  @m%c%05d";
+
+const char STR_800C45C4_cn[] = "\n\n@c2Wid@c7";
+
+const char STR_800C45D0_cn[] = "@c2OnVrs  Rensa  MRensa HiEr    WallP   ErOL3\n@c7 @m%c%05d  @m%c%05d  @m%c%05d  @m%c%05d%%  @m%c%02d:%-3s  @m%c%05d\n\n";
+
+const char STR_800C4648_cn[] = "@c2\tLine2  Line3  Line4~8\n@c2Hei@c7";
+
+const char STR_800C466C_cn[] = "\n\n@c2DebugSpeed PlayerCom\n@c7 @m%c%03d        @m%c%03d:%-14s\n\n\n\n\n";
+
+const char STR_800C46B0_cn[] = "Man";
+
+const char STR_800C46B4_cn[] = "@c3<- SpeedMenu\t\t\t\t\tCharacterMenu ->";
+
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026550_cn", func_8004168C_cn);
+
+ASM_RODATA;
+
+const char STR_800C4710_cn[] = "@c2Character         WaitAttack Speed       Luck\n@c7 @m%c%02d:%-14s @m%c%02d:%-3s     @m%c%02d:%-8s @m%c%03d%%\n\n";
+
+const char STR_800C4784_cn[] = "@c2%-18s %-18s\n@c7 @m%c%02d:%-14s  @m%c%02d:%-14s\n\n";
+
+const char STR_800C47B8_cn[] = "@c2  ExCondition        Sub   ExEffect       Sub\n";
+
+const char STR_800C47EC_cn[] = "@c2%02d @c7@m%c%03d:%-14s ";
+
+const char STR_800C4808_cn[] = "@m%c---   ";
+
+const char STR_800C4814_cn[] = "@m%c%03d%c  ";
+
+const char STR_800C4824_cn[] = "@m%c%03d:%-10s ";
+
+const char STR_800C4834_cn[] = "@m%c---";
+
+const char STR_800C483C_cn[] = "@m%c%03d";
+
+const char STR_800C4848_cn[] = "\n";
+
+const char STR_800C484C_cn[] = "@c3<- LogicMenu\t\t\t\t\t\tSpeedMenu ->";
+
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026550_cn", func_80041918_cn);
+
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026550_cn", func_80041CF8_cn);
+
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026550_cn", func_80041FC0_cn);
+
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026550_cn", func_8004206C_cn);
+
+
+ASM_RODATA;
+
+const char STR_800C48A0_cn[] = "踢剌窘诉傲";
+
+const char STR_800C48AC_cn[] = "腐蔬";
+
+const u8 STR_800C48B4_cn[] = {
+    0xB6, 0xD2, 0xAF, 0xB8, 
+    0x00, 0x00, 0x00, 0x00, 
+};
+
+const u8 STR_800C48BC_cn[] = {
+    0xCA, 0xDF, 0xAF, 0xB8, 
+    0xDD, 0xCC, 0xD7, 0xDC, 
+    0xB0, 0x00, 0x00, 0x00, 
+};
+
+const u8 STR_800C48C8_cn[] = {
+    0xC3, 0xDA, 0xBB, 0x00, 
+};
+
+const u8 STR_800C48CC_cn[] = {
+    0xCA, 0xC5, 0xC1, 0xAC, 
+    0xDD, 0x00, 0x00, 0x00, 
+};
+
+const char STR_800C48D4_cn[] = "踢弁咦筒伟";
+
+const char STR_800C48E0_cn[] = "廉厶甙";
+
+const char STR_800C48E8_cn[] = "踢柑吒";
+
+const u8 STR_800C48F0_cn[] = {
+    0xCE, 0xDE, 0xD1, 0xCD, 
+    0xB2, 0x00, 0x00, 0x00, 
+};
+
+const char STR_800C48F8_cn[] = "珊珊";
+
+const u8 STR_800C4900_cn[] = {
+    0xCF, 0xD8, 0xB5, 0x00, 
+};
+
+const u8 STR_800C4904_cn[] = {
+    0xD6, 0xBA, 0xBD, 0xDE, 
+    0xB7, 0x00, 0x00, 0x00, 
+};
+
+const u8 STR_800C490C_cn[] = {
+    0xC0, 0xDE, 0xB2, 0xDA, 
+    0xDD, 0xBB, 0xC8, 0xD7, 
+    0xB2, 0x00, 0x00, 0x00, 
+};
+
+const u8 STR_800C4918_cn[] = {
+    0xBA, 0xB3, 0xB9, 0xDE, 
+    0xB7, 0x00, 0x00, 0x00, 
+};
+
+const u8 STR_800C4920_cn[] = {
+    0xCA, 0xDE, 0xD7, 0xDD, 
+    0xBD, 0x00, 0x00, 0x00, 
+};
+
+const char STR_800C4928_cn[] = "剿甙霓";
+
+const u8 STR_800C4930_cn[] = {
+    0xD3, 0xAF, 0xC4, 0xD3, 
+    0xCA, 0xD4, 0xB2, 0x00, 
+};
+
+const u8 STR_800C4938_cn[] = {
+    0xCB, 0xBC, 0xDE, 0xAE, 
+    0xB3, 0xC6, 0xCA, 0xD4, 
+    0xB2, 0x00, 0x00, 0x00, 
+};
+
+const u8 STR_800C4944_cn[] = {
+    0xCA, 0xD4, 0xB2, 0x00, 
+};
+
+const u8 STR_800C4948_cn[] = {
+    0xCC, 0xC2, 0xB3, 0x00, 
+};
+
+const u8 STR_800C494C_cn[] = {
+    0xB5, 0xBF, 0xB2, 0x00, 
+};
+
+const u8 STR_800C4950_cn[] = {
+    0xCB, 0xBC, 0xDE, 0xAE, 
+    0xB3, 0xC6, 0xB5, 0xBF, 
+    0xB2, 0x00, 0x00, 0x00, 
+};
+
+const char STR_800C495C_cn[] = "棉太倌";
+
+const char STR_800C4964_cn[] = "挡盒";
+
+const u8 STR_800C496C_cn[] = {
+    0xC3, 0xC7, 0xB7, 0x00, 
+};
+
+const char STR_800C4970_cn[] = "挡惫拶";
+
+const char STR_800C4978_cn[] = "剿甙霓悲踢";
+
+const u8 STR_800C4984_cn[] = {
+    0xC4, 0xDE, 0xB3, 0xD6, 
+    0xB3, 0x00, 0x00, 0x00, 
+};
+
+const u8 STR_800C498C_cn[] = {
+    0xC1, 0xAE, 0xB3, 0xCA, 
+    0xC2, 0x00, 0x00, 0x00, 
+};
+
+const u8 STR_800C4994_cn[] = {
+    0xB8, 0xD9, 0xB8, 0xD9, 
+    0xCF, 0xDC, 0xBD, 0x00, 
+};
+
+const u8 STR_800C499C_cn[] = {
+    0xCF, 0xD6, 0xB2, 0x00, 
+};
+
+const u8 STR_800C49A0_cn[] = {
+    0xD7, 0xAF, 0xB8, 0x00, 
+};
+
+const u8 STR_800C49A4_cn[] = {
+    0xB6, 0xB3, 0xDD, 0xC0, 
+    0xB0, 0x00, 0x00, 0x00, 
+};
+
+const u8 STR_800C49AC_cn[] = {
+    0xBE, 0xDD, 0xBE, 0xB2, 
+    0xBA, 0xB3, 0xB9, 0xDE, 
+    0xB7, 0x00, 0x00, 0x00, 
+};
+
+
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026550_cn", func_800421E0_cn);
+
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026550_cn", func_80042478_cn);
+
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026550_cn", func_80042598_cn);
+
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026550_cn", func_80042980_cn);
+
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026550_cn", func_80042A58_cn);
+
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026550_cn", func_80042E78_cn);
+
+INCLUDE_ASM("asm/cn/nonmatchings/main_segment/026550_cn", func_80042EDC_cn);
+
+ASM_RODATA;
+
+const char STR_800C49E8_cn[] = "<母辑>";
+
+const char STR_800C49F0_cn[] = "<剿甙霓>";
+
+const char STR_800C49FC_cn[] = "<静陡>";
+
+const char STR_800C4A04_cn[] = "<番赘腊>";
+
+const u8 STR_800C4A10_cn[] = {
+    0x40, 0x63, 0x32, 0x5B, 
+    0xB7, 0xAC, 0xD7, 0xB8, 
+    0xC0, 0xB0, 0xB4, 0xC3, 
+    0xDE, 0xA8, 0xAF, 0xC4, 
+    0x5D, 0x0A, 0x0A, 0x00, 
+};
+
+const u8 STR_800C4A24_cn[] = {
+    0x40, 0x63, 0x32, 0xC2, 
+    0xD6, 0xBB, 0x20, 0x00, 
+};
+
+const char STR_800C4A2C_cn[] = "%c";
+
+const char STR_800C4A30_cn[] = "@c2%s\n";
+
+const char STR_800C4A38_cn[] = "@m%c@c%c%s\n";
+
+const char STR_800C4A44_cn[] = "@m%c@c%c%c:%s\n";
+#endif
