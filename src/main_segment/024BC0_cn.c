942428cee8aa31e727836114736eca1043ecf2ec
angie <angheloalf95@gmail.com>
2023-01-31 11:59:04 -0300

    forgot versions #ifs

--- src/main_segment/024BC0_cn.c
+++ src/main_segment/024BC0_cn.c
@@ -3,8 +3,14 @@
 #include "macros_defines.h"
 #include "unknown_structs.h"
 
+#if VERSION_CN
 INCLUDE_ASM("asm/cn/nonmatchings/main_segment/024BC0_cn", func_80040020_cn);
+#endif
 
+#if VERSION_CN
 INCLUDE_ASM("asm/cn/nonmatchings/main_segment/024BC0_cn", func_80040060_cn);
+#endif
 
+#if VERSION_CN
 INCLUDE_ASM("asm/cn/nonmatchings/main_segment/024BC0_cn", func_8004008C_cn);
+#endif
