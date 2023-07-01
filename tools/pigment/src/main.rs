1bd0c1fc7a54f6ccd39631f6e6062c6d0c1c47ff
angie <angheloalf95@gmail.com>
2023-07-01 11:08:19 -0400

    git subrepo pull tools/pigment
    subrepo:
      subdir:   "tools/pigment"
      merged:   "e48e272b"
    upstream:
      origin:   "git@github.com:decompals/pigment.git"
      branch:   "main"
      commit:   "e48e272b"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "2f68596"

--- tools/pigment/src/main.rs
+++ tools/pigment/src/main.rs
@@ -1,7 +1,8 @@
 use clap::{Parser, ValueEnum};
 use std::fs::File;
+use std::io::{self, prelude::*};
 use std::io::{BufReader, BufWriter};
-use std::io::prelude::*;
+use std::mem;
 use std::path::PathBuf;
 
 /// PNG to N64 image converter
@@ -26,6 +27,14 @@ struct Args {
     /// Flip the image on the y axis
     #[arg(long)]
     flip_y: bool,
+
+    /// Output a raw C array which can be `#include`d in a file. The default output type width matches the FORMAT provided, but it can be overridden with --type-width
+    #[arg(short, long)]
+    c_array: bool,
+
+    /// Overrides the natural fit of each format when outputting a C array
+    #[arg(short, long, value_enum)]
+    type_width: Option<TypeWidthArray>,
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, ValueEnum, Debug)]
@@ -42,6 +51,65 @@ enum Format {
     Palette,
 }
 
+#[derive(Copy, Clone, PartialEq, Eq, ValueEnum, Debug)]
+enum TypeWidthArray {
+    U8,
+    U16,
+    U32,
+    U64,
+}
+
+impl Format {
+    fn get_width(&self) -> TypeWidthArray {
+        match self {
+            Format::Ci4 => TypeWidthArray::U8,
+            Format::Ci8 => TypeWidthArray::U8,
+            Format::I4 => TypeWidthArray::U8,
+            Format::I8 => TypeWidthArray::U8,
+            Format::Ia4 => TypeWidthArray::U8,
+            Format::Ia8 => TypeWidthArray::U8,
+            Format::Ia16 => TypeWidthArray::U16,
+            Format::Rgba16 => TypeWidthArray::U16,
+            Format::Rgba32 => TypeWidthArray::U32,
+            Format::Palette => TypeWidthArray::U16,
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! write_buf_as_raw_array {
+    ($dst:expr, $bin:expr, $type_width:ident) => {
+        let width = mem::size_of::<$type_width>();
+
+        for row in $bin.chunks(16) {
+            let mut line_list = Vec::new();
+            for bytes in row.chunks(width) {
+                let value = $type_width::from_be_bytes(bytes.try_into().unwrap());
+
+                line_list.push(format!("0x{value:00$X}", 2 * width));
+            }
+            let line = line_list.join(", ");
+            write!($dst, "    {line},\n").expect("could not write to output file");
+        }
+    };
+}
+
+fn write_buf_as_u8(output_file: &mut Box<dyn Write>, bin: &Vec<u8>) {
+    write_buf_as_raw_array!(output_file, bin, u8);
+}
+
+fn write_buf_as_u16(output_file: &mut Box<dyn Write>, bin: &Vec<u8>) {
+    write_buf_as_raw_array!(output_file, bin, u16);
+}
+
+fn write_buf_as_u32(output_file: &mut Box<dyn Write>, bin: &Vec<u8>) {
+    write_buf_as_raw_array!(output_file, bin, u32);
+}
+
+fn write_buf_as_u64(output_file: &mut Box<dyn Write>, bin: &Vec<u8>) {
+    write_buf_as_raw_array!(output_file, bin, u64);
+}
+
 fn main() {
     let args = Args::parse();
 
@@ -53,11 +121,11 @@ fn main() {
         pigment::get_palette_rgba16(&mut input_reader)
     } else {
         let mut image = pigment::Image::read_png(&mut input_reader);
-        
+
         if args.flip_x || args.flip_y {
             image = image.flip(args.flip_x, args.flip_y);
         }
-        
+
         match args.format {
             Format::Ci4 => image.as_ci4(),
             Format::Ci8 => image.as_ci8(),
@@ -72,13 +140,39 @@ fn main() {
         }
     };
 
-    let output_path = PathBuf::from(args.output.unwrap_or_else(|| {
-        let mut path = args.input.clone();
-        path.push_str(".bin");
-        path
-    }));
+    let mut output_file: Box<dyn Write>;
 
-    BufWriter::new(File::create(output_path).expect("could not open output file"))
-        .write_all(&bin)
-        .expect("could not write to output file");
+    if args.c_array && args.output.is_none() {
+        output_file = Box::from(io::stdout());
+    } else {
+        let output_path = PathBuf::from(args.output.unwrap_or_else(|| {
+            let mut path = args.input.clone();
+            if args.c_array {
+                path.push_str(".inc.c");
+            } else {
+                path.push_str(".bin");
+            }
+            path
+        }));
+
+        output_file = Box::from(File::create(output_path).unwrap());
+    }
+
+    if args.c_array {
+        let mut type_width = args.format.get_width();
+
+        // Override if the user passed the appropriate flag
+        type_width = args.type_width.unwrap_or(type_width);
+
+        match type_width {
+            TypeWidthArray::U8 => write_buf_as_u8(&mut output_file, &bin),
+            TypeWidthArray::U16 => write_buf_as_u16(&mut output_file, &bin),
+            TypeWidthArray::U32 => write_buf_as_u32(&mut output_file, &bin),
+            TypeWidthArray::U64 => write_buf_as_u64(&mut output_file, &bin),
+        }
+    } else {
+        BufWriter::new(output_file)
+            .write_all(&bin)
+            .expect("could not write to output file");
+    }
 }
--- tools/pigment/tests/test.rs
+++ tools/pigment/tests/test.rs
@@ -4,60 +4,60 @@ use pigment::*;
 // TODO: convert input into all permutations of color type and bit depth
 // to test all codepaths
 
-#[test]
-fn ci8() -> Result<(), Box<dyn std::error::Error>> {
-    let input_bytes = include_bytes!("ci8.png");
-    let image = Image::read_png(&mut Cursor::new(input_bytes), false, false);
+// #[test]
+// fn ci8() -> Result<(), Box<dyn std::error::Error>> {
+//     let input_bytes = include_bytes!("ci8.png");
+//     let image = Image::read_png(&mut Cursor::new(input_bytes));
 
-    let expected_bytes = include_bytes!("ci8.png.bin");
-    assert_eq!(image.as_ci8(), expected_bytes);
-    Ok(())
-}
+//     let expected_bytes = include_bytes!("ci8.png.bin");
+//     assert_eq!(image.as_ci8(), expected_bytes);
+//     Ok(())
+// }
 
-#[test]
-fn ci4() -> Result<(), Box<dyn std::error::Error>> {
-    let input_bytes = include_bytes!("ci4.png");
-    let image = Image::read_png(&mut Cursor::new(input_bytes), false, false);
+// #[test]
+// fn ci4() -> Result<(), Box<dyn std::error::Error>> {
+//     let input_bytes = include_bytes!("ci4.png");
+//     let image = Image::read_png(&mut Cursor::new(input_bytes));
 
-    let expected_bytes = include_bytes!("ci4.png.bin");
-    assert_eq!(image.as_ci4(), expected_bytes);
-    Ok(())
-}
+//     let expected_bytes = include_bytes!("ci4.png.bin");
+//     assert_eq!(image.as_ci4(), expected_bytes);
+//     Ok(())
+// }
 
-#[test]
-fn i4() -> Result<(), Box<dyn std::error::Error>> {
-    let input_bytes = include_bytes!("i4.png");
-    let image = Image::read_png(&mut Cursor::new(input_bytes), false, false);
+// #[test]
+// fn i4() -> Result<(), Box<dyn std::error::Error>> {
+//     let input_bytes = include_bytes!("i4.png");
+//     let image = Image::read_png(&mut Cursor::new(input_bytes));
 
-    let expected_bytes = include_bytes!("i4.png.bin");
-    assert_eq!(image.as_i4(), expected_bytes);
-    Ok(())
-}
+//     let expected_bytes = include_bytes!("i4.png.bin");
+//     assert_eq!(image.as_i4(), expected_bytes);
+//     Ok(())
+// }
 
 #[test]
 fn i8() -> Result<(), Box<dyn std::error::Error>> {
     let input_bytes = include_bytes!("i8.png");
-    let image = Image::read_png(&mut Cursor::new(input_bytes), false, false);
+    let image = Image::read_png(&mut Cursor::new(input_bytes));
 
     let expected_bytes = include_bytes!("i8.png.bin");
     assert_eq!(image.as_i8(), expected_bytes);
     Ok(())
 }
 
-#[test]
-fn ia4() -> Result<(), Box<dyn std::error::Error>> {
-    let input_bytes = include_bytes!("ia4.png");
-    let image = Image::read_png(&mut Cursor::new(input_bytes), false, false);
+// #[test]
+// fn ia4() -> Result<(), Box<dyn std::error::Error>> {
+//     let input_bytes = include_bytes!("ia4.png");
+//     let image = Image::read_png(&mut Cursor::new(input_bytes));
 
-    let expected_bytes = include_bytes!("ia4.png.bin");
-    assert_eq!(image.as_ia4(), expected_bytes);
-    Ok(())
-}
+//     let expected_bytes = include_bytes!("ia4.png.bin");
+//     assert_eq!(image.as_ia4(), expected_bytes);
+//     Ok(())
+// }
 
 #[test]
 fn ia8() -> Result<(), Box<dyn std::error::Error>> {
     let input_bytes = include_bytes!("ia8.png");
-    let image = Image::read_png(&mut Cursor::new(input_bytes), false, false);
+    let image = Image::read_png(&mut Cursor::new(input_bytes));
 
     let expected_bytes = include_bytes!("ia8.png.bin");
     assert_eq!(image.as_ia8(), expected_bytes);
@@ -67,7 +67,7 @@ fn ia8() -> Result<(), Box<dyn std::error::Error>> {
 #[test]
 fn ia16() -> Result<(), Box<dyn std::error::Error>> {
     let input_bytes = include_bytes!("ia16.png");
-    let image = Image::read_png(&mut Cursor::new(input_bytes), false, false);
+    let image = Image::read_png(&mut Cursor::new(input_bytes));
 
     let expected_bytes = include_bytes!("ia16.png.bin");
     assert_eq!(image.as_ia16(), expected_bytes);
@@ -77,7 +77,7 @@ fn ia16() -> Result<(), Box<dyn std::error::Error>> {
 #[test]
 fn rgba16() -> Result<(), Box<dyn std::error::Error>> {
     let input_bytes = include_bytes!("rgba16.png");
-    let image = Image::read_png(&mut Cursor::new(input_bytes), false, false);
+    let image = Image::read_png(&mut Cursor::new(input_bytes));
 
     let expected_bytes = include_bytes!("rgba16.png.bin");
     assert_eq!(image.as_rgba16(), expected_bytes);
@@ -87,19 +87,19 @@ fn rgba16() -> Result<(), Box<dyn std::error::Error>> {
 #[test]
 fn rgba32() -> Result<(), Box<dyn std::error::Error>> {
     let input_bytes = include_bytes!("rgba32.png");
-    let image = Image::read_png(&mut Cursor::new(input_bytes), false, false);
+    let image = Image::read_png(&mut Cursor::new(input_bytes));
 
     let expected_bytes = include_bytes!("rgba32.png.bin");
     assert_eq!(image.as_rgba32(), expected_bytes);
     Ok(())
 }
 
-#[test]
-fn palette() -> Result<(), Box<dyn std::error::Error>> {
-    let input_bytes = include_bytes!("ci8.png");
-    let palette = get_palette_rgba16(&mut Cursor::new(input_bytes));
+// #[test]
+// fn palette() -> Result<(), Box<dyn std::error::Error>> {
+//     let input_bytes = include_bytes!("ci8.png");
+//     let palette = get_palette_rgba16(&mut Cursor::new(input_bytes));
 
-    let expected_bytes = include_bytes!("ci8.pal.bin");
-    assert_eq!(palette, expected_bytes);
-    Ok(())
-}
+//     let expected_bytes = include_bytes!("ci8.pal.bin");
+//     assert_eq!(palette, expected_bytes);
+//     Ok(())
+// }
