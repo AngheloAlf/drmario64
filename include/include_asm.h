e40fdfea5406ddc7bd0cfd80bcd5681b15112290
angie <angheloalf95@gmail.com>
2024-05-12 12:28:05 -0400

    Cleanup include_asm.h

--- include/include_asm.h
+++ include/include_asm.h
@@ -6,28 +6,20 @@
 #define INCLUDE_ASM(FOLDER, NAME) \
     __asm__( \
         ".section .text\n" \
-        "\t.set noat\n" \
-        "\t.set noreorder\n" \
-        "\t.align\t2\n" \
-        "\t.globl\t"#NAME"\n" \
-        "\t.type "#NAME", @function\n" \
-        "\t.ent\t"#NAME"\n" \
-        #NAME ":\n" \
-        "\t.include \""FOLDER"/"#NAME".s\"\n" \
-        "\t.set reorder\n" \
-        "\t.set at\n" \
-        "\t.end\t"#NAME"\n" \
-        ".end"#NAME":\n" \
-        "\t.size\t"#NAME",.end"#NAME"-"#NAME "\n" \
-        "\t.globl\t" #NAME ".NON_MATCHING\n" \
-        "\t" #NAME ".NON_MATCHING" " = " #NAME "\n" \
+        "    .set noat\n" \
+        "    .set noreorder\n" \
+        "    .include \"" FOLDER "/" #NAME ".s\"\n" \
+        "    .set reorder\n" \
+        "    .set at\n" \
+        "    .globl " #NAME ".NON_MATCHING\n" \
+        "    " #NAME ".NON_MATCHING" " = 0x0" "\n" \
     )
 #endif
 #ifndef INCLUDE_RODATA
 #define INCLUDE_RODATA(FOLDER, NAME) \
     __asm__( \
         ".section .rodata\n" \
-        "\t.include \""FOLDER"/"#NAME".s\"\n" \
+        "    .include \"" FOLDER "/" #NAME ".s\"\n" \
         ".section .text" \
     )
 #endif
